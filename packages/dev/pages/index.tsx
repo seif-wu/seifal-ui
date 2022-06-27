bfa304706113bc568e9fc8a619a4b3f84f944b60
Seif Wu <seif.wuxf@gmail.com>
2022-06-27 22:27:36 +0800

    docs(dev): add use button code

--- packages/dev/pages/index.tsx
+++ packages/dev/pages/index.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import { Button } from '@seifal/ui';
-import { ThemeProvider, useTheme } from '@seifal/system';
+import { ThemeProvider } from '@seifal/system';
 
 const Home: NextPage = () => {
-  // const t = useTheme()
-  // console.log(t)
   return (
-    <>
-      <Button>哈哈哈哈</Button>
-    </>
-    // <ThemeProvider theme={{}}>
-    // </ThemeProvider>
+    <ThemeProvider theme={{}}>
+      <Button type="primary" block>哈哈哈哈</Button>
+    </ThemeProvider>
   );
 };
 
